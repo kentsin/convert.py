fdbbbd17ca546eb182237e34e980ad89037efe39
kentsin <kentsin@gmail.com>
2022-07-09 09:31:12 +0800

    original

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 import os			    # for magick and tesseract commands
 import time			  # for epoch time
 import calendar 	# for epoch time
-#from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfFileMerger
 
 dir_files = [f for f in os.listdir(".") if os.path.isfile(os.path.join(".", f))]
 epoch_time = int(calendar.timegm(time.gmtime()))
@@ -11,21 +11,14 @@ for file in dir_files: # look at every file in the current directory
 	if file.endswith('.pdf'): # if it is a PDF, use it
 		print('Working on converting: ' + file)
 		# setup
-		#file = file.replace('.pdf', '') # get just the filepath without the extension
-		file = file[:-4]
-		#folder = str(int(epoch_time)) + '_' + file # generate a folder name for temporary images
-		folder = 'F_' + file
+		file = file.replace('.pdf', '') # get just the filepath without the extension
+		folder = str(int(epoch_time)) + '_' + file # generate a folder name for temporary images
 		combined = folder + '/' + file # come up with temporary export path
 		# create folder
 		if not os.path.exists(folder): # make the temporary folder
 			os.makedirs(folder)
-		# convert PDF to PNG(s) 
-		# You can combine two -crops:
-		#                        left,top      right,bottom
-		# convert test.png -crop +180+140 -crop -60-140 cropped.png
-		# 
-		# # https://stackoverflow.com/a/50495343
-		magick = 'convert -density 150 -crop +55+180 -crop -55-128 "' + file + '.pdf" "' + combined + '-%04d.png"'
+		# convert PDF to PNG(s)
+		magick = 'convert -density 150 "' + file + '.pdf" "' + combined + '-%04d.png"'
 		print(magick)
 		os.system(magick)
 		# convert PNG(s) to PDF(s) with OCR data
@@ -34,16 +27,16 @@ for file in dir_files: # look at every file in the current directory
 			if pic.endswith('.png'):
 				combined_pic = folder + '/' + pic
 				print(combined_pic)
-				tesseract = 'tesseract -l chi_tra+por "' + combined_pic + '" "' + combined_pic + '.txt'
+				tesseract = 'tesseract "' + combined_pic + '" "' + combined_pic + '-ocr" PDF'
 				print(tesseract)
 				os.system(tesseract)
 		# combine OCR'd PDFs into one
 		ocr_pdfs = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
 
-		#merger = PdfFileMerger()
-		#for pdf in ocr_pdfs:
-		#	if pdf.endswith('.pdf'):
-		#		merger.append(folder + '/' + pdf)
+		merger = PdfFileMerger()
+		for pdf in ocr_pdfs:
+			if pdf.endswith('.pdf'):
+				merger.append(folder + '/' + pdf)
 
-		#merger.write(file + '-ocr-combined.pdf')
-		#merger.close()
+		merger.write(file + '-ocr-combined.pdf')
+		merger.close()
